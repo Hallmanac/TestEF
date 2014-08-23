4cc60044445ecd5c4ff208d8a4144bbd6eb5005d
Hallmanac <Brian@Hallmanac.com>
2014-08-23 13:57:21 -0400

    Implemented ITenant

--- TestEf.Console/Identity/User.cs
+++ TestEf.Console/Identity/User.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using TestEf.Console.Core;
+using TestEf.Console.Tenant;
 
 namespace TestEf.Console.Identity
 {
-    public class User : IBaseEntity
+    public class User : IBaseEntity, ITenant
     {
         public User()
         {
@@ -12,23 +13,30 @@ namespace TestEf.Console.Identity
             PhoneNumbers = new List<PhoneNumber>();
         }
 
-        public int Id { get; set; }
-
         public string FirstName { get; set; }
 
         public string LastName { get; set; }
 
-        public List<Email> Emails { get; set; } 
+        public string Username { get; set; }
 
-        public List<PhoneNumber> PhoneNumbers { get; set; } 
+        public List<Email> Emails { get; set; }
+
+        public List<PhoneNumber> PhoneNumbers { get; set; }
+        public int Id { get; set; }
 
         public DateTimeOffset LastModifiedOn { get; set; }
+
+        /// <summary>
+        /// Tenant Id for hosting multi-tenant applications
+        /// </summary>
+        public int TenantId { get; set; }
 
         public bool IsIdenticalTo(User entity)
         {
             return entity.Id == Id
                    && entity.FirstName == FirstName
                    && entity.LastName == LastName
+                   && TenantId == entity.TenantId
                    && entity.LastModifiedOn == LastModifiedOn;
         }
     }
