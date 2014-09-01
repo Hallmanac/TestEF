3d255b293fd06a00405dde5fd9b7197245366686
Hallmanac <Brian@Hallmanac.com>
2014-08-31 20:16:53 -0400

    Streamlined the ToBatch extension method

--- TestEf.Console/Core/ExtensionMethods/ListExtensions.cs
+++ TestEf.Console/Core/ExtensionMethods/ListExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace TestEf.Console.Core.ExtensionMethods
 {
@@ -7,22 +8,16 @@ namespace TestEf.Console.Core.ExtensionMethods
         public static List<List<T>> ToBatch<T>(this List<T> currentList, int batchSize)
         {
             var batchList = new List<List<T>>();
-
             var maxBatchCount = currentList.Count < batchSize ? currentList.Count : batchSize;
-            var remainingCount = currentList.Count;
-            while (remainingCount != 0)
+            var currentCount = 0;
+            while(currentCount < currentList.Count)
             {
                 var batch = new List<T>();
-                for (var i = remainingCount; i > remainingCount - maxBatchCount; i--)
-                {
-                    batch.Add(currentList[i - 1]);
-                }
+                batch.AddRange(currentList.Skip(currentCount).Take(maxBatchCount).ToList());
                 batchList.Add(batch);
-                remainingCount = remainingCount - maxBatchCount;
-                if (maxBatchCount > remainingCount)
-                    maxBatchCount = remainingCount;
+                currentCount += maxBatchCount;
             }
             return batchList;
-        }  
+        }
     }
 }
